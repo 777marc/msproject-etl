9bb7406dd2e9a5e6b74bc7df410142d5ef7ad0c2
Marc Mendoza <777marc@gmail.com>
2022-10-27 15:55:19 -0400

    init

--- lib/msproject-etl-stack.ts
+++ lib/msproject-etl-stack.ts
@@ -1,16 +1,33 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as path from "path";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3n from "aws-cdk-lib/aws-s3-notifications";
 export class MsprojectEtlStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const lambdaFunction = new lambda.Function(this, "lambda-function", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: "index.main",
+      code: lambda.Code.fromAsset(path.join(__dirname, "/../src/lambda")),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MsprojectEtlQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const s3Bucket = new s3.Bucket(this, "s3-bucket", {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    s3Bucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED,
+      new s3n.LambdaDestination(lambdaFunction)
+      // only invoke lambda if object matches the filter
+      // {prefix: 'test/', suffix: '.yaml'},
+    );
+
+    new cdk.CfnOutput(this, "bucketName", {
+      value: s3Bucket.bucketName,
+    });
   }
 }
